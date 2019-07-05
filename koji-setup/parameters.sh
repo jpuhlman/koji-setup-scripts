71db5d7ad7de28859df5c9fe56f98166a36f4086
Jeremy Puhlman <jpuhlman@mvista.com>
2019-07-05 15:08:54 -0700

    Inital internal setup

--- koji-setup/parameters.sh
+++ koji-setup/parameters.sh
@@ -5,31 +5,35 @@
 ## KOJI RPM BUILD AND TRACKER
 export KOJI_DIR=/srv/koji
 export KOJI_MOUNT_DIR=/mnt/koji
-export KOJI_MASTER_FQDN="$(hostname -f)"
+
+export KOJI_MASTER_FQDN="$HOST"
+if [ -z "$KOJI_MASTER_FQDN" ] ; then
+	exit 1
+fi
 export KOJI_URL=https://"$KOJI_MASTER_FQDN"
 export KOJI_MOUNT_DIR=/mnt/koji
 export KOJI_SLAVE_FQDN="$KOJI_MASTER_FQDN"
 export KOJID_CAPACITY=16
-export TAG_NAME=clear
+export TAG_NAME=centos-updates
 # Use for koji SSL certificates
-export COUNTRY_CODE='Example Country Code'
-export STATE='Example State'
-export LOCATION='Example Location'
-export ORGANIZATION='Example Organization'
-export ORG_UNIT='Example Org Unit'
+export COUNTRY_CODE='US'
+export STATE='California'
+export LOCATION='Santa Clara'
+export ORGANIZATION='Montavista'
+export ORG_UNIT='MV'
 # Use for importing existing RPMs
 export RPM_ARCH='x86_64'
 export SRC_RPM_DIR=
 export BIN_RPM_DIR=
 export DEBUG_RPM_DIR=
 # Comment the following if supplying all RPMs as an upstream and not a downstream
-export EXTERNAL_REPO=https://cdn.download.clearlinux.org/releases/"$(curl https://download.clearlinux.org/latest)"/clear/\$arch/os/
+export EXTERNAL_REPO=http://vault.centos.org/7.5.1804/os/x86_64/
 
 ## POSTGRESQL DATABASE
 export POSTGRES_DIR=/srv/pgsql
 
 ## GIT REPOSITORIES
-export GIT_DIR=/srv/gitolite
+export GIT_DIR=/srv/git
 export GIT_FQDN="$KOJI_MASTER_FQDN"
 export IS_ANONYMOUS_GIT_NEEDED=false
 export GITOLITE_PUB_KEY=''
