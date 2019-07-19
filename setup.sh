f2b1ab67569d09fe005453fa8370f4b86331aa9c
jenkins <jenkins@jptest03.mvista.com>
2019-07-19 13:07:39 -0700

    Fix up initalization scripts.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
-echo -n "Root " 
-su -c "mkdir -e /etc/sudoers.d/; echo 'jenkins ALL=(ALL) NOPASSWD: ALL' | tee -a /etc/sudoers.d/visudo"
-
-sudo ./koji-setup/deploy-koji.sh
-sudo ./koji-setup/bootstrap-build.sh
+#!/bin/bash -e
+if false; then
+if [ -z "$(sudo grep "^jenkins\ " /etc/sudoers.d/visudo 2>/dev/null)" ] ; then
+   echo -n "Root " 
+   su -c "mkdir -p /etc/sudoers.d/; echo 'jenkins ALL=(ALL) NOPASSWD: ALL' | tee -a /etc/sudoers.d/visudo"
+fi
+sudo -E ./koji-setup/deploy-koji.sh
+sudo -E ./koji-setup/bootstrap-build.sh
 sudo -u kojiadmin koji moshimoshi
-source ./koji-setup/parameters.sh
 
+source ./koji-setup/parameters.sh
 pushd /etc/pki/koji/
 sudo ./gencert.sh jenkins "/C=$COUNTRY_CODE/ST=$STATE/L=$LOCATION/O=$ORGANIZATION/OU=$ORG_UNIT/CN=jenkins"
 sudo cp /home/kojiadmin/.koji/config /tmp/config-koji
@@ -14,23 +16,25 @@ popd
 
 mkdir -p ~/.koji && cd $_
 cp /etc/pki/koji/jenkins.pem .
-cp -v clear.pem client.crt
+cp -v jenkins.pem client.crt
 cp /etc/pki/koji/koji_ca_cert.crt .
 cp -v koji_ca_cert.crt clientca.crt
 cp -v koji_ca_cert.crt serverca.crt
-/tmp/config-koji ./config
+cd ~/.koji
+cp /tmp/config-koji ./config
 sudo mkdir -p /etc/ca-certs/trusted
 sudo cp serverca.crt /etc/ca-certs/trusted/
 sudo clrtrust generate
 koji moshimoshi
 
 pushd ~/koji-setup-scripts/
-sudo ./koji-setup/deploy-mash.sh
+sudo -E ./koji-setup/deploy-mash.sh
 systemctl status mash
+else
 
-exit 1
 sudo docker build jenkins-mv -t jenkins-mv:latest
 popd
 mkdir -p jenkins
 cp -a .koji jenkins/
 sudo mkdir -p /srv/jenkins
+fi
