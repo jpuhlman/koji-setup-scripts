a3121cdb2d90ec44d02e5f45e49562c1c95f7bc2
Jenkins <jenkins@mvista.com>
2019-07-22 12:27:28 -0700

    instrement build-rpm.sh

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 pushd ~
 sudo swupd bundle-add os-clr-on-clr
@@ -7,8 +7,12 @@ chmod +x user-setup.sh
 ./user-setup.sh
 git config --global user.email "jenkins@mvista.com"
 git config --global user.name "Jenkins"
+pwd
 cd clearlinux
+pwd
 make clone_rpm PKG_BASE_URL=git://gitcentos.mvista.com/centos/upstream/utils
+pwd
 cd packages/rpm
+pwd
 make build
 sudo rpm -ihv --force --nodeps rpms/*.rpm
